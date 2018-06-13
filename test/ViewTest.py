2f3de039baa7ebe3f4b2808e9024813e4c4a5f83
a <tomasz_91@o2.pl>
2018-06-13 18:13:39 +0200

    Fixed formatting
    Change-Id: Iff230fbefd455eb039ef427103315380f175d450

--- test/ViewTest.py
+++ test/ViewTest.py
@@ -5,11 +5,15 @@ from src.View import *
 
 class TestGeneral(unittest.TestCase):
     def test_loadFromSingleMonitorTest(self):
-        x = View(2);
-        monitor = '0.0.0.0';
+        x = View(2)
+        monitor = '0.0.0.0'
         measurements = x.prepare(
-            {'CPU_USAGE': [[monitor, 'workstation1', 2], [monitor, 'workstation2', 3], [monitor, 'workstation3', 1]]})
-        self.assertEqual(measurements, {'CPU_USAGE': [[monitor, 'workstation2', 3], [monitor, 'workstation1', 2]]});
+            {'CPU_USAGE': [[monitor, 'workstation1', 2],
+                           [monitor, 'workstation2', 3],
+                           [monitor, 'workstation3', 1]]})
+        self.assertEqual(measurements,
+            {'CPU_USAGE': [[monitor, 'workstation2', 3],
+                           [monitor, 'workstation1', 2]]})
 
 
 if __name__ == '__main__':
