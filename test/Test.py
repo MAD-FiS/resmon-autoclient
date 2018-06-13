423224a60630544d8d296488549ecd3c26b33c1c
a <tomasz_91@o2.pl>
2018-06-13 18:52:20 +0200

    Fixed UT
    Change-Id: I57d9aecf4f12dbe3ab78a3c1390db4db37f80912

--- test/Test.py
+++ test/Test.py
@@ -1,7 +1,7 @@
 import unittest
 
 from src.MeasurementsCollection import *
-
+from src.View import *
 
 class TestGeneral(unittest.TestCase):
     def test_loadFromSingleMonitorTest(self):
@@ -22,6 +22,17 @@ class TestGeneral(unittest.TestCase):
         self.assertEqual(measurements,
             {'CPU_USAGE': [[monitor, 'workstation1', 1], [monitor, 'workstation2', 2]],
              'RAM_USAGE': [[monitor, 'workstation1', 100]]})
+
+    def test_prepare(self):
+        x = View(2)
+        monitor = '0.0.0.0'
+        measurements = x.prepare(
+            {'CPU_USAGE': [[monitor, 'workstation1', 2],
+                           [monitor, 'workstation2', 3],
+                           [monitor, 'workstation3', 1]]})
+        self.assertEqual(measurements,
+            {'CPU_USAGE': [[monitor, 'workstation2', 3],
+                           [monitor, 'workstation1', 2]]})
 
 if __name__ == '__main__':
     unittest.main()
